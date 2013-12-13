8ff11ff4e1460efc0a249dd85b54b4129b97d385
antoine <antoine.muller@etu.unistra.fr>
2013-12-13 14:13:06 +0100

    Ajout architecture

---
+++ WEB-INF/src/TomDog.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: antoine
+ * Date: 13/12/13
+ * Time: 14:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class TomDog
+{
+    public static void main(String args[])
+    {
+
+    }
+}
